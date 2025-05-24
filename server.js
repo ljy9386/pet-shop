814cc1bd6b6fe24e370770d7dbc0994c0815e161
진영 <lim@jin-yeong-ui-MacBookPro.local>
2025-05-24 17:13:01 +0900

    🎉 Miracle Pet Shop 첫 업로드

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000 에서 실행 중');
+});
