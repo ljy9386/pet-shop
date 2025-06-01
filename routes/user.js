2832e6da91666d56077c7eba3f1a329e1ca36d6d
ljy9386 <ljy9386@naver.com>
2025-06-02 00:11:31 +0900

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); // ✅ 기존 모델 재사용
 
+router.get("/check/:userId", async (req, res) => {
+  const user = await User.findOne({ user_id: req.params.userId });
+  res.json({ exists: !!user });
+});
+
 module.exports = router;
