6a1c3f323c93697be5df7d64aafe0e007d71f170
ljy9386 <ljy9386@naver.com>
2025-06-05 17:16:02 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   user_id: { type: String, required: true, unique: true },
-  ser_id: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+  ser_id: { type: String, required: false, unique: true },
+  email: { type: String, required: false, unique: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
   address: { type: String, required: true },
