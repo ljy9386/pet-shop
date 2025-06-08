86be99563c1cbd7e3d10278f623cb29f1ba43562
CheolPark98 <96028198+EscapeSleep@users.noreply.github.com>
2025-06-08 13:26:25 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   user_id: { type: String, required: true, unique: true },
+  
   email: { type: String, required: false, unique: true, sparse: true},
   name: { type: String, required: true },
   password: { type: String, required: true },
